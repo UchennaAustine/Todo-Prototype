9eb5997e39f17336e0ca74fc3b83b7053a33ac71
UchennaAustine <uchennaaustine8@gmail.com>
2023-07-03 23:27:03 +0100

    extra

--- src/Components/Common/Header.tsx
+++ src/Components/Common/Header.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <Container>
+        <Main>
+          <NavHold>
+            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+              <Nav>Home</Nav>
+            </Link>
+          </NavHold>
 
-export default Header
+          <Logo>Uchenna Austine</Logo>
+
+          <Link to="/Input" style={{ textDecoration: "none", color: "black" }}>
+            <Button>Add Task</Button>
+          </Link>
+        </Main>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;
+
+const Button = styled.div`
+  padding: 10px 18px;
+  background-color: white;
+  border-radius: 3px;
+  color: black;
+  cursor: pointer;
+  font-weight: 500;
+`;
+
+const Logo = styled.div`
+  font-weight: 700;
+  font-size: 18px;
+`;
+
+const Nav = styled.div`
+  font-weight: 500;
+`;
+
+const NavHold = styled.div``;
+
+const Main = styled.div`
+  width: 90%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  color: white;
+  background-color: black;
+`;
